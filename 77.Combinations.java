c0161845690b3c31e1c27a9838a1d4926268d756
Dan <17553473+poname@users.noreply.github.com>
2023-10-17 17:26:53 -0400

    optimized solution

--- 77.Combinations.java
+++ 77.Combinations.java
@@ -1,4 +1,26 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> cur = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        rec(1, n, k, cur, ans);
+        return ans;
+    }
 
+    private void rec(int i, int n, int k, List<Integer> cur, List<List<Integer>> ans) {
+        if (cur.size() == k) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+        if (i > n) {
+            return;
+        }
+        for (int j=i; j<=n; j++) {
+            cur.add(j);
+            rec(j+1, n, k, cur, ans);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
 
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
