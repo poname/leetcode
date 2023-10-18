d760968c3f0ad3dd81f5e1366ad5edb6dc386138
Dan <17553473+poname@users.noreply.github.com>
2023-10-18 12:49:47 -0400

    iterative approach added

--- 46.Permutations.java
+++ 46.Permutations.java
@@ -1,5 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+
+        for (int n : nums) {
+            List<List<Integer>> temp = new ArrayList<>();
+            for (List<Integer> a : ans) {
+                for (int i=0; i<=a.size(); i++) {
+                    List<Integer> aClone = new ArrayList<>(a);
+                    aClone.add(i, n);
+                    temp.add(aClone);
+                }
+            }
+            ans = temp;
+        }
+
+        return ans;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
         return rec(0, nums);
     }
 
