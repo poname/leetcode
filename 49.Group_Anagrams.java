4af7c44ebafe11aeeabf2c3ed86afe84bb04ec40
Dan <17553473+poname@users.noreply.github.com>
2023-10-23 16:44:10 -0400

    49. Group Anagrams

---
+++ 49.Group_Anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            String sorted = sort(s);
+            map.putIfAbsent(sorted, new ArrayList<>());
+            map.get(sorted).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    private String sort(String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        return new String(c); 
+    }
+}
