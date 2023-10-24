74c7adb972aadeb2b5202f53233313b9a4b41385
Dan <17553473+poname@users.noreply.github.com>
2023-10-24 13:45:28 -0400

    array bucket added

--- 347.Top_K_Frequent_Elements.java
+++ 347.Top_K_Frequent_Elements.java
@@ -5,6 +5,36 @@ class Solution {
             map.putIfAbsent(n, 0);
             map.computeIfPresent(n, (a,b) -> b+1);
         }
+        ArrayList<Integer>[] bucket = new ArrayList[nums.length+1];
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (bucket[e.getValue()] == null) {
+                bucket[e.getValue()] = new ArrayList<>();
+            }
+            bucket[e.getValue()].add(e.getKey());
+        }
+        int[] out = new int[k];
+        for (int i=nums.length; i>0; i--) {
+            if (bucket[i] != null) {
+                for (int j=0; j<bucket[i].size(); j++) {
+                    out[--k] = bucket[i].get(j);
+                    if (k==0) {
+                        return out;
+                    }
+                }
+                
+            }
+        }
+        return out;
+    }
+}
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.putIfAbsent(n, 0);
+            map.computeIfPresent(n, (a,b) -> b+1);
+        }
         Map<Integer, List<Integer>> rev = new HashMap<>();
         for (Map.Entry<Integer, Integer> e : map.entrySet()) {
             rev.putIfAbsent(e.getValue(), new ArrayList<>());
