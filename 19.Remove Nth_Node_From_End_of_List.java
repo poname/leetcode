90793fdf78e8c6d90bf46c3f8d39b0284edf7ece
Dan <17553473+poname@users.noreply.github.com>
2023-11-09 18:08:02 -0500

    single pass

--- 19.Remove Nth_Node_From_End_of_List.java
+++ 19.Remove Nth_Node_From_End_of_List.java
@@ -1,10 +1,32 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy;
+        ListNode right = head;
+
+        while (n > 0) {
+            right = right.next;
+            n--;
+        }
+
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+        
+        return dummy.next;
+    }
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         int count = 0;
         for(ListNode curr=head; curr!=null; curr=curr.next) {
             count++;
         }
-            int diff = count-n;
+        int diff = count-n;
         ListNode curr=head;
         ListNode prev=null;
         while (diff > 0) {
