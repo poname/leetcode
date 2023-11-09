b922905267ad54ddb29bd743c04427284be33ceb
Dan <17553473+poname@users.noreply.github.com>
2023-11-08 19:31:30 -0500

    424. Longest Repeating Character Replacement

---
+++ 424.Longest_Repeating_Character_Replacement.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int l=0;
+        char[] ss = s.toCharArray();
+        int len = 0;
+        int maxFreq = 0;
+        for (int r=0; r<ss.length; r++) {
+            count[ss[r]-'A']++;
+            maxFreq = Math.max(maxFreq, count[ss[r]-'A']);
+            while (r-l+1-maxFreq > k) {
+                count[ss[l]-'A']--;
+                l++;
+            }
+            len = Math.max(len, r-l+1);
+        }
+        return len;
+    }
+}
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int l=0;
+        char[] ss = s.toCharArray();
+        int len = 0;
+        for (int r=0; r<ss.length; r++) {
+            count[ss[r]-'A']++;
+            while (r-l+1-getMax(count) > k) {
+                count[ss[l]-'A']--;
+                l++;
+            }
+            len = Math.max(len, r-l+1);
+        }
+        return len;
+    }
+    private int getMax(int[] count) {
+        int max = count[0];
+        for (int i : count) {
+            max = Math.max(max, i);
+        }
+        return max;
+    }
+}
