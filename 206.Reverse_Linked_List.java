36bfc8b1ae5d81a0e17a9f1ea7c8663fa7d6be16
Dan <17553473+poname@users.noreply.github.com>
2023-11-09 12:57:47 -0500

    recursive added

--- 206.Reverse_Linked_List.java
+++ 206.Reverse_Linked_List.java
@@ -1,6 +1,25 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = null;
+        if (head.next == null) {
+            newHead = head;
+        } else {
+            newHead =  reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+        }
+        
+        return newHead;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
             return head;
         }
         ListNode prev=null, cur=head;
