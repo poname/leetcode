6aff1b285d07c1af1ae1135f5cc0f4fe7dd4aa88
Dan <17553473+poname@users.noreply.github.com>
2023-11-10 11:22:18 -0500

    iterative solution added

--- 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -1,5 +1,19 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+    }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
         } else if (root.val < p.val && root.val < q.val) {
