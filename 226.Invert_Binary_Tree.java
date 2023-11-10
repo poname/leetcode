f66f9f45ecb087f6c0b0430e5c5e246b2f145fc1
Dan <17553473+poname@users.noreply.github.com>
2023-11-09 19:13:30 -0500

    Update 226.Invert_Binary_Tree.java

--- 226.Invert_Binary_Tree.java
+++ 226.Invert_Binary_Tree.java
@@ -3,6 +3,23 @@ class Solution {
         if (root == null) {
             return null;
         }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.right);
+        invertTree(root.left);
+
+        return root;
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
         TreeNode left = invertTree(root.right);
         TreeNode right = invertTree(root.left);
         root.left = left;
