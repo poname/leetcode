dd3fea3d31768366755db08cfd173167ce7b5830
Dan <17553473+poname@users.noreply.github.com>
2023-11-11 10:36:58 -0500

    bfs added

--- 200.Number_of_Islands.java
+++ 200.Number_of_Islands.java
@@ -7,12 +7,30 @@ class Solution {
         for (int r=0; r<rows; r++) {
             for (int c=0; c<cols; c++) {
                 if (grid[r][c]=='1' && !visited[r][c]) {
-                    islands++;
-                    dfs(r, c, visited, grid);
+                   islands++;
+                //    bfs(r, c, visited, grid);
+                   dfs(r, c, visited, grid);
                 }
             }
         }
         return islands;
+    }
+    private void bfs(int row, int col, boolean[][] visited, char[][] grid) {
+        Queue<Integer[]> queue = new LinkedList<>();
+        queue.add(new Integer[]{row, col});
+        while (queue.size() > 0) {
+            Integer[] top = queue.remove();
+            int r = top[0];
+            int c = top[1];
+            if (r<0 || r>=grid.length || c<0 || c>= grid[0].length || visited[r][c] || grid[r][c]=='0') {
+                continue;
+            }
+            visited[r][c] = true;
+            queue.add(new Integer[]{r+1, c});
+            queue.add(new Integer[]{r-1, c});
+            queue.add(new Integer[]{r, c+1});
+            queue.add(new Integer[]{r, c-1});
+        }
     }
     private void dfs(int r, int c, boolean[][] visited, char[][] grid) {
         if (r<0 || r>=grid.length || c<0 || c>= grid[0].length || visited[r][c] || grid[r][c]=='0') {
