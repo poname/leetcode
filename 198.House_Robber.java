088c834d9f0726426ae396d404bcdf90eafaaedd
Dan <17553473+poname@users.noreply.github.com>
2023-11-11 18:49:13 -0500

    optimized memory

--- 198.House_Robber.java
+++ 198.House_Robber.java
@@ -4,6 +4,23 @@ class Solution {
         if (n==1) {
             return nums[0];
         }
+        int a = nums[0];
+        int b = Math.max(nums[0], nums[1]);
+        for (int i=2; i<n; i++) {
+            int c = Math.max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n==1) {
+            return nums[0];
+        }
         int[] dp = new int[n];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
