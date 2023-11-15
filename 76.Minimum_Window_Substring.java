2828ceca4b46bd6bc9c90f02322a0864cb28d971
Dan <17553473+poname@users.noreply.github.com>
2023-11-15 16:27:00 -0500

    optimized

--- 76.Minimum_Window_Substring.java
+++ 76.Minimum_Window_Substring.java
@@ -2,6 +2,47 @@ class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> need = new HashMap<>();
         Map<Character, Integer> have = new HashMap<>();
+        for (Character c : t.toCharArray()) {
+            need.putIfAbsent(c, 0);
+            have.putIfAbsent(c, 0);
+            need.computeIfPresent(c, (k,v) -> v+1);
+        }
+
+        int sat = 0; // satisfied constraints
+        
+        char[] in = s.toCharArray();
+        int minLen = in.length+1;
+        int minStart = -1;
+        int l = 0;
+        for (int r=0; r<in.length; r++) {
+            have.computeIfPresent(in[r], (k,v) -> v+1);
+            if (need.containsKey(in[r]) && have.get(in[r]).equals(need.get(in[r]))) {
+                sat++;
+            }
+            while (sat==need.size()) {
+                if (r-l+1 < minLen) {
+                    minLen = r-l+1;
+                    minStart = l;
+                }
+                if (need.containsKey(in[l]) && have.get(in[l]).equals(need.get(in[l]))) {
+                    sat--;
+                }
+                have.computeIfPresent(in[l], (k,v) -> v-1);
+                l++;
+            }
+        }
+        if (minStart < 0) {
+            return "";
+        } else {
+            return s.substring(minStart, minStart+minLen);
+        }
+    }
+}
+
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> have = new HashMap<>();
         for (char c : t.toCharArray()) {
             need.putIfAbsent(c, 0);
             have.putIfAbsent(c, 0);
