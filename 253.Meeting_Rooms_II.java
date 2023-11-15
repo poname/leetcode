58f7c42706d74bbeb9c7218ee4a15cb0ef259280
Dan <17553473+poname@users.noreply.github.com>
2023-11-14 20:40:44 -0500

    Two Pointer solution

--- 253.Meeting_Rooms_II.java
+++ 253.Meeting_Rooms_II.java
@@ -37,3 +37,53 @@ public class Solution {
         return max;
     }
 }
+
+/**
+ * Two Pointer
+ */
+
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: the minimum number of conference rooms required
+     */
+    public int minMeetingRooms(List<Interval> intervals) {
+        if (intervals == null || intervals.size() == 0) {
+            return 0;
+        }
+        int n = intervals.size();
+        // Write your code here
+        int[] start = new int[n];
+        int[] end = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            Interval iv = intervals.get(i);
+            start[i] = iv.start;
+            end[i] = iv.end;
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int s=0;
+        int e=0;
+        int curr = 0;
+        int max = 0;
+        while (s<n && e<n) {
+            if (start[s] < end[e]) {
+                curr++;
+                s++;
+                max = Math.max(max, curr);
+            } else if (start[s] > end[e]) {
+                curr--;
+                e++;
+                max = Math.max(max, curr);
+            } else {
+                s++;
+                e++;
+            }
+        }
+
+        return max;
+    }
+}
