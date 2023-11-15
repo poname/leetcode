29bf6b100e725d63e0be0ff7a696ad87f376ee62
Dan <17553473+poname@users.noreply.github.com>
2023-11-15 09:49:54 -0500

    direct solution

--- 48.Rotate_Image.java
+++ 48.Rotate_Image.java
@@ -1,6 +1,37 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        int l = 0;
+        int r = n-1;
+        while (l<r) {
+            for (int i=0; i<r-l; i++) {
+                int top = l;
+                int bottom = r;
+
+                // save top left
+                int topLeft = matrix[top][l + i];
+
+                // move bottom left to top left
+                matrix[top][l + i] = matrix[bottom - i][l];
+
+                // move borrom right to bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i];
+
+                // move top right to bottom right
+                matrix[bottom][r - i] = matrix[top + i][r];
+
+                // move top left tp top right
+                matrix[top + i][r] = topLeft; 
+            }
+            l++;
+            r--;
+        }
+    }
+}
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
         // transpose
         for (int i=0; i<n; i++) {
             for (int j=i+1; j<n; j++) {
