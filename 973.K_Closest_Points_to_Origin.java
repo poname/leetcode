834ca15ac7f30d8b96796dd67d981184ed415c5d
Dan <17553473+poname@users.noreply.github.com>
2023-11-20 16:00:01 -0500

    typo

--- 973.K_Closest_Points_to_Origin.java
+++ 973.K_Closest_Points_to_Origin.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Queue<int[]> minHeap = new PriorityQueue<>(k, (a,b) -> (b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
+        Queue<int[]> maxHeap = new PriorityQueue<>(k, (a,b) -> (b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
         for (int[] p : points) {
-            minHeap.add(p);
-            if (minHeap.size() > k) {
-                minHeap.remove();
+            maxHeap.add(p);
+            if (maxHeap.size() > k) {
+                maxHeap.remove();
             }
         }
         int[][] arr = new int[k][2];
         int i = 0;
-        for (int[] c : minHeap) {
+        for (int[] c : maxHeap) {
             arr[i][0] = c[0];
             arr[i][1] = c[1];
             i++;
