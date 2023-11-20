2f19be08246747fd194e3eb880805160efe1861e
Dan <17553473+poname@users.noreply.github.com>
2023-11-20 11:08:52 -0500

    653. Two Sum IV

---
+++ 653.Two_Sum_IV.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+        bst(root, arr);
+        int l=0;
+        int r=arr.size()-1;
+        while (l<r) {
+            int sum = arr.get(l) + arr.get(r);
+            if (sum > k) {
+                r--;
+            } else if (sum < k) {
+                l++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    private void bst(TreeNode root, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        bst(root.left, arr);
+        arr.add(root.val);
+        bst(root.right, arr);
+    }
+}
