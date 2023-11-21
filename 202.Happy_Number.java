34856124df4145fec2120d78b79d60d93bc2bed9
Dan <17553473+poname@users.noreply.github.com>
2023-11-21 12:01:29 -0500

    optimized slow/fast pointers

--- 202.Happy_Number.java
+++ 202.Happy_Number.java
@@ -1,5 +1,29 @@
 class Solution {
     public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        while (fast != 1) {
+            slow = ssq(slow);
+            fast = ssq(ssq(fast));
+            if (slow == fast) {
+                return fast==1;
+            }
+        }
+        return fast==1;
+    }
+    int ssq(int n) {
+        int s = 0;
+        while (n > 0) {
+            int d = n%10;
+            s += d*d;
+            n /= 10;
+        }
+        return s;
+    }
+}
+
+class Solution {
+    public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
         while (!set.contains(n) && n!=1) {
             set.add(n);
