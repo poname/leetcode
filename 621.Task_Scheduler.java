fab4d7fad188ddb4ebfb3b0254248bbc540ce54c
Dan <17553473+poname@users.noreply.github.com>
2023-11-23 11:51:53 -0500

    Update 621.Task_Scheduler.java

--- 621.Task_Scheduler.java
+++ 621.Task_Scheduler.java
@@ -1,5 +1,36 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        Queue<Integer> maxHeap = new PriorityQueue<>((x, y) -> y-x); // remaining count
+        Queue<Integer[]> wait = new PriorityQueue<>((x, y) -> x[0]-y[0]); // [0]: next time, [1]: count
+        int[] count = new int[26];
+        for (char t : tasks) {
+            count[t-'A']++;
+        }
+        for (int t : count) {
+            if (t > 0) {
+                maxHeap.add(t);
+            }
+        }
+        int time = 0;
+        while (maxHeap.size() > 0 || wait.size() > 0) {
+            if (maxHeap.size() > 0) {
+                int curr = maxHeap.remove();
+                curr--;
+                if (curr > 0) {
+                    wait.add(new Integer[]{time+n+1, curr});
+                }
+            }
+            while (wait.size() > 0 && wait.peek()[0] <= time+1) {
+                maxHeap.add(wait.remove()[1]);
+            }
+            time++;
+        }
+        return time;
+    }
+}
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
         Queue<Task> maxHeap = new PriorityQueue<>((x, y) -> y.count-x.count);
         Queue<Task> wait = new PriorityQueue<>((x, y) -> x.next-y.next);
         Map<Character, Task> counter = new HashMap<>();
