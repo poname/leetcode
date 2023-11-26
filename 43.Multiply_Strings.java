0f7ce61df57e5e81280789d90ec4f4d816f9f79e
Dan <17553473+poname@users.noreply.github.com>
2023-11-26 17:55:18 -0500

    optimized solution

--- 43.Multiply_Strings.java
+++ 43.Multiply_Strings.java
@@ -5,6 +5,32 @@ class Solution {
         }
         String in1 = num1;
         String in2 = num2;
+        int[] sum = new int[in1.length() + in2.length() + 1]; // + 1 for dummy carry in
+
+        for (int i=in1.length()-1; i>=0; i--) {
+            for (int j=in2.length()-1; j>=0; j--) {
+                sum[i+j+1] += (in1.charAt(i)-'0') * (in2.charAt(j)-'0') + sum[i+j+2]/10; // pick carry from prev digit
+                sum[i+j+2] %= 10; // set prev digit to single digit sum
+            } 
+        }
+        // System.out.println(Arrays.toSt)
+        StringBuilder sb = new StringBuilder();
+        for (int i=1; i<sum.length; i++) {
+            if (i<sum.length-1 || sum[i]!=0) {
+                sb.append(sum[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        String in1 = num1;
+        String in2 = num2;
         if (num1.length() < num2.length()) {
             in2 = num1;
             in1 = num2;
