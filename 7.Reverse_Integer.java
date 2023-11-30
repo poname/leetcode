9c036ef2fc164de77f2790eec1b2a503e336b6f8
Dan <17553473+poname@users.noreply.github.com>
2023-11-30 15:36:33 -0500

    7. Reverse Integer

---
+++ 7.Reverse_Integer.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int reverse(int x) {
+        char[] min = String.valueOf(Integer.MIN_VALUE).toCharArray();
+        char[] max = String.valueOf(Integer.MAX_VALUE).toCharArray();
+        char[] num = String.valueOf(x).toCharArray();
+        int n = num.length;
+        char[] rev = new char[n];
+        if (num[0] == '-') {
+            rev[0] = '-';
+            for (int i=1; i<n; i++) {
+                rev[n-i] = num[i];
+            }
+            if (rev.length == min.length) {
+                for (int i=0; i<n; i++) {
+                    if (rev[i] > min[i]) {
+                        return 0;
+                    } else if (rev[i] < min[i]) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            return Integer.valueOf(new String(rev));
+        } else {
+            for (int i=0; i<n; i++) {
+                rev[n-1-i] = num[i];
+            }
+            if (rev.length == max.length) {
+                for (int i=0; i<n; i++) {
+                    if (rev[i] > max[i]) {
+                        return 0;
+                    } else if (rev[i] < max[i]) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            return Integer.valueOf(new String(rev));
+        }
+    }
+}
