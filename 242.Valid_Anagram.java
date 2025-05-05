d9ccc22d15697c5289ab7a8fa015a86c367c55a9
Dan <17553473+poname@users.noreply.github.com>
2025-05-05 17:19:11 -0400

    Update 242.Valid_Anagram.java

--- 242.Valid_Anagram.java
+++ 242.Valid_Anagram.java
@@ -1,3 +1,5 @@
+// time: O(n)
+// space: O(1)
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] count = new int[26];
@@ -13,5 +15,18 @@ class Solution {
             }
         }
         return true;
+    }
+}
+
+// time: O(nlog(n))
+// space: O(n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        return Arrays.equals(ss, tt);
     }
 }
