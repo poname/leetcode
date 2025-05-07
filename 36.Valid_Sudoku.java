148f6a93bea954829d093ba94d3cf6a5c8a7df85
Dan <17553473+poname@users.noreply.github.com>
2025-05-07 14:56:18 -0400

    Update 36.Valid_Sudoku.java

--- 36.Valid_Sudoku.java
+++ 36.Valid_Sudoku.java
@@ -31,3 +31,45 @@ class Solution {
         return true;
     }
 }
+
+// time: O(n^2)
+// space: O(1)
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int r=0; r<9; r++) {
+            int[] count = new int[10];
+            for (int c=0; c<9; c++) {
+                char m = board[r][c];
+                if (m != '.') {
+                    if (count[m-'0'] > 0) return false;
+                    count[m-'0']++;
+                }
+            }
+        }
+        for (int c=0; c<9; c++) {
+            int[] count = new int[10];
+            for (int r=0; r<9; r++) {
+                char m = board[r][c];
+                if (m != '.') {
+                    if (count[m-'0'] > 0) return false;
+                    count[m-'0']++;
+                }
+            }
+        }
+        for (int x=0; x<3; x++) {
+            for (int y=0; y<3; y++) {
+                int[] count = new int[10];
+                for (int r=3*x; r<3*x+3; r++) {
+                    for (int c=3*y; c<3*y+3; c++) {
+                        char m = board[r][c];
+                        if (m != '.') {
+                            if (count[m-'0'] > 0) return false;
+                            count[m-'0']++;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
