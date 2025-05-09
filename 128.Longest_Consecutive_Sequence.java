5682188486c15ff798898365586d3fc106e80ea7
Dan <17553473+poname@users.noreply.github.com>
2025-05-09 08:45:44 -0400

    Update 128.Longest_Consecutive_Sequence.java

--- 128.Longest_Consecutive_Sequence.java
+++ 128.Longest_Consecutive_Sequence.java
@@ -1,3 +1,5 @@
+// time: O(n)
+// space: O(n)
 class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -12,6 +14,58 @@ class Solution {
                     tail++;
                 }
                 max = Math.max(max, tail-k+1);
+            }
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        int max = 0;
+        for (int s : set) {
+            if (!set.contains(s-1)) {
+                int count = 1;
+                int head = s;
+                while (set.contains(head+1)) {
+                    count++;
+                    head++;
+                }
+                max = Math.max(count, max);
+            }
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, false);
+        }
+        int max = 0;
+        for (int n : nums) {
+            if (!map.get(n)) {
+                int count = 1;
+                map.put(n, true);
+                int left = n-1;
+                while (map.containsKey(left)) {
+                    map.put(left, true);
+                    count++;
+                    left--;
+                }
+                int right = n+1;
+                while (map.containsKey(right)) {
+                    map.put(right, true);
+                    count++;
+                    right++;
+                }
+                max = Math.max(max, count);
             }
         }
         return max;
