4cdd7db9b11a933f4c6e1f310e2c07bab6bf9caa
Dan <17553473+poname@users.noreply.github.com>
2025-05-12 18:01:12 -0400

    Update 155.Min_Stack.java

--- 155.Min_Stack.java
+++ 155.Min_Stack.java
@@ -1,4 +1,34 @@
 class MinStack {
+    Stack<Integer> stack, min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);
+        min = new Stack<>();
+        min.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        min.push(Math.min(min.peek(), val));
+    }
+    
+    public void pop() {
+        stack.pop();
+        min.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
+
+
+class MinStack {
 
     Stack<Integer> stack;
     Stack<Integer> min;
