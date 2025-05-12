3f45613320f96020c560a741603ec28743f94db8
Dan <17553473+poname@users.noreply.github.com>
2025-05-12 17:17:10 -0400

    Update 20.Valid_Parentheses.java

--- 20.Valid_Parentheses.java
+++ 20.Valid_Parentheses.java
@@ -1,3 +1,28 @@
+// time: O(n)
+// space: O(n)
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length()%2 == 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.add(c);
+            } else {
+                if (stack.size()==0) return false;
+                if (c == ')') {
+                    if (stack.peek() != '(') return false;   
+                } else if (c == '}') {
+                    if (stack.peek() != '{') return false;   
+                } else if (c == ']') {
+                    if (stack.peek() != '[') return false;   
+                }
+                stack.pop();
+            }
+        }
+        return stack.size() == 0;
+    }
+}
+
 class Solution {
     public boolean isValid(String s) {
         if (s.length() % 2 == 1) {
