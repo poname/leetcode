6e5e4634873321f1bcc053663665a7fae240e49e
Dan <17553473+poname@users.noreply.github.com>
2025-05-12 14:58:59 -0400

    Update 271.Encode_and_Decode_Strings.java

--- 271.Encode_and_Decode_Strings.java
+++ 271.Encode_and_Decode_Strings.java
@@ -1,3 +1,41 @@
+// time: O()
+// space: O()
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        // write your code here
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s.length());
+            sb.append(":");
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        // write your code here
+        int s=0;
+        int e=0;
+        List<String> ans = new ArrayList<>();
+        while (s<str.length()) {
+            while (str.charAt(e) != ':') e++;
+            int len = Integer.parseInt(str.substring(s, e));
+            ans.add(str.substring(e+1, e+len+1));
+            s = e+len+1;
+            e = e+len+1;
+        }
+        return ans;
+    }
+}
+
 public class Solution {
     /*
      * @param strs: a list of strings
