1b8932ffa40fecd20bb316a650598226007e3826
Dan <17553473+poname@users.noreply.github.com>
2025-05-13 17:39:29 -0400

    array solution added

--- 150.Evaluate_Reverse_Polish_Notation.java
+++ 150.Evaluate_Reverse_Polish_Notation.java
@@ -1,3 +1,39 @@
+// no stack - space optimized
+// time: O(n)
+// space: O(n)
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int[] stack = new int[(tokens.length+1)/2];
+        int top=0;
+        for (String s : tokens) {
+            char t = s.charAt(0);
+            if(t=='+') {
+                int op2 = Integer.valueOf(stack[--top]);
+                int op1 = Integer.valueOf(stack[--top]);
+                stack[top++] = op1 + op2;
+            } else if (t=='-' && s.length()==1) {
+                int op2 = Integer.valueOf(stack[--top]);
+                int op1 = Integer.valueOf(stack[--top]);
+                stack[top++] = op1 - op2;
+            } else if (t=='*') {
+                int op2 = Integer.valueOf(stack[--top]);
+                int op1 = Integer.valueOf(stack[--top]);
+                stack[top++] = op1 * op2;
+            } else if (t=='/') {
+                int op2 = Integer.valueOf(stack[--top]);
+                int op1 = Integer.valueOf(stack[--top]);
+                stack[top++] = op1 / op2;
+            } else {
+                stack[top++] = Integer.valueOf(s);
+            }
+        }
+        return stack[0];
+    }
+}
+
+// stack
+// time: O(n)
+// space: O(n)
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
