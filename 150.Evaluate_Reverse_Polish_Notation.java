80ec08f81e58a397735628312453da118fffadad
Dan <17553473+poname@users.noreply.github.com>
2025-05-15 13:56:14 -0400

    Update 150.Evaluate_Reverse_Polish_Notation.java

--- 150.Evaluate_Reverse_Polish_Notation.java
+++ 150.Evaluate_Reverse_Polish_Notation.java
@@ -1,3 +1,33 @@
+// recursive - dfs - fastest
+// time: O(n)
+// space: O(1)
+class Solution {
+    public int evalRPN(String[] tokens) {
+        return dfs(new int[]{tokens.length-1}, tokens);
+    }
+    int dfs(int[] i, String[] tokens) {
+        String t = tokens[i[0]];
+        char s = t.charAt(0);
+        i[0]--;
+        if (t.length()>1 || (s!='+' && s!='-' && s!='*' && s!='/')) {
+            return Integer.valueOf(t);
+        }
+        int op2 = dfs(i, tokens);
+        int op1 = dfs(i, tokens);
+        if (s=='+') {
+            return op1+op2;
+        } else if (s=='-') {
+            return op1-op2;
+        } else if (s=='*') {
+            return op1*op2;
+        } else if (s=='/') {
+            return op1/op2;
+        } else {
+            return 0;
+        }
+    }
+}
+
 // no stack - space optimized
 // time: O(n)
 // space: O(n)
