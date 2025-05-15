c82fea751fb0cce23e517d77dd50724c482f5fd1
Dan <17553473+poname@users.noreply.github.com>
2025-05-15 19:53:42 -0400

    Update 167.Two_Sum_II.java

--- 167.Two_Sum_II.java
+++ 167.Two_Sum_II.java
@@ -1,5 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        int l=0, r=numbers.length-1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) break;
+            else if (sum > target) r--;
+            else l++;
+        }
+        return new int[]{l+1, r+1};
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
         int l=0;
         int r=numbers.length-1;
         while (l<r) {
