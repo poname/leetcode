05c93edd9106955057e412f34eb5d1bba17766a6
Dan <17553473+poname@users.noreply.github.com>
2025-05-16 10:03:39 -0400

    Update 15.3Sum.java

--- 15.3Sum.java
+++ 15.3Sum.java
@@ -1,3 +1,32 @@
+// time: O(n^2)
+// space: O(n)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> out = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
+            int l = i + 1;
+            int r = nums.length - 1;
+            int target = -1 * nums[i];
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum > target)
+                    r--;
+                else if (sum < target)
+                    l++;
+                else {
+                    out.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+                    while (l<nums.length && nums[l-1]==nums[l]) l++;
+                    while (r>=0 && nums[r]==nums[r+1]) r--;
+                }
+            }
+        }
+        return out;
+    }
+}
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
