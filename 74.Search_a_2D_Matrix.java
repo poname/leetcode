5368bc8c519b22ea65477c05f2c074fe5b40ce25
Dan <17553473+poname@users.noreply.github.com>
2025-05-16 14:39:21 -0400

    Update 74.Search_a_2D_Matrix.java

--- 74.Search_a_2D_Matrix.java
+++ 74.Search_a_2D_Matrix.java
@@ -1,3 +1,22 @@
+// iterative
+// time: O(log(m*n))
+// space: O(1)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length, n=matrix[0].length;
+        int start = 0, end = m*n-1;
+        while (start <= end) {
+            int mid = (start+end)/2;
+            int mR = mid/n, mC = mid%n;
+            if (matrix[mR][mC] == target) return true;
+            else if (matrix[mR][mC] < target) start = mid+1;
+            else   end = mid-1;
+        }
+        return false;
+    }
+}
+
+// recursive
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
