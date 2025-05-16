36b9763e3060e56e9dfd6bf90993d91cfc51edaa
Dan <17553473+poname@users.noreply.github.com>
2025-05-16 13:01:52 -0400

    Update 42.Trapping_Rain_Water.java

--- 42.Trapping_Rain_Water.java
+++ 42.Trapping_Rain_Water.java
@@ -1,3 +1,29 @@
+// 3 pass
+// time: O(n)
+// space: O(n)
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] ltr = new int[n];
+        int[] rtl = new int[n];
+        int maxR = 0;
+        for (int i=0; i<n; i++) {
+            ltr[i] = maxR;
+            maxR = Math.max(maxR, height[i]);
+        }
+        int maxL = 0;
+        for (int i=n-1; i>=0; i--) {
+            rtl[i] = maxL;
+            maxL = Math.max(maxL, height[i]);
+        }
+        int vol = 0;
+        for (int i=0; i<n; i++) {
+            vol += Math.max(0, Math.min(rtl[i], ltr[i])-height[i]);
+        }
+        return vol;
+    }
+}
+
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
