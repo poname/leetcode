6496c67c595fe2beea8a28db65ee8708572e700f
Dan <17553473+poname@users.noreply.github.com>
2025-05-16 14:25:52 -0400

    Update 704.Binary_Search.java

--- 704.Binary_Search.java
+++ 704.Binary_Search.java
@@ -17,6 +17,17 @@ class Solution {
 // recurive
 class Solution {
     public int search(int[] nums, int target) {
+        return dfs(0, nums.length-1, nums, target);
+    }
+    int dfs(int start, int end, int[] nums, int target) {
+        if (start > end) return -1;
+        int mid = (start+end)/2;
+        if (nums[mid] == target) return mid;
+        return (nums[mid]<target) ? dfs(mid+1, end, nums, target) : dfs(start, mid-1, nums, target);
+    }
+}
+class Solution {
+    public int search(int[] nums, int target) {
         return bst(0, nums.length-1, target, nums);
     }
     
