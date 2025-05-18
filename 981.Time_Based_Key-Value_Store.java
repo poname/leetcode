a1e2e310b68e41d6031ed65dca8a4cca5825610d
Dan <17553473+poname@users.noreply.github.com>
2025-05-18 13:48:28 -0400

    Update 981.Time_Based_Key-Value_Store.java

--- 981.Time_Based_Key-Value_Store.java
+++ 981.Time_Based_Key-Value_Store.java
@@ -1,3 +1,47 @@
+// 
+class TimeMap {
+
+    Map<String, List<Pair>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.putIfAbsent(key, new ArrayList<>());
+        map.get(key).add(new Pair(value, timestamp));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key))
+            return "";
+        List<Pair> values = map.get(key);
+        System.out.println(values.size());
+        int l = 0, r = values.size() - 1;
+        String ret = "";
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (values.get(m).timestamp <= timestamp) {
+                ret = values.get(m).value;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ret;
+    }
+}
+
+class Pair {
+    String value;
+    int timestamp;
+
+    public Pair(String value, int timestamp) {
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+}
+
 class TimeMap {
     Map<String, List<Pair>> map;
 
