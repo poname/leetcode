22660a11ce303c6b4c22adb47678a8ffb36d6c10
Dan <17553473+poname@users.noreply.github.com>
2025-05-18 23:51:31 -0400

    Update 121.Best_Time_to_Buy_and_Sell_Stock.java

--- 121.Best_Time_to_Buy_and_Sell_Stock.java
+++ 121.Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,3 +1,35 @@
+// time: O(n)
+// space: O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            profit += prices[i] - prices[i - 1];
+            if (profit < 0)
+                profit = 0;
+            max = Math.max(max, profit);
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int max = 0;
+        int l = 0, r = 1;
+        while (r < prices.length) {
+            profit = prices[r] - prices[l];
+            if (profit < 0)
+                l = r;
+            max = Math.max(max, profit);
+            r++;
+        }
+        return max;
+    }
+}
+
 class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
