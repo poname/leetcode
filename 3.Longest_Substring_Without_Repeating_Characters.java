a59998222236f71a0805d09e555c98cea929ce00
Dan <17553473+poname@users.noreply.github.com>
2025-05-19 01:02:47 -0400

    Update 3.Longest_Substring_Without_Repeating_Characters.java

--- 3.Longest_Substring_Without_Repeating_Characters.java
+++ 3.Longest_Substring_Without_Repeating_Characters.java
@@ -1,3 +1,35 @@
+// time: O(n) - n is the length of the string
+// space: O(m) - m is the number of unique characters in the string
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] set = new int[128];
+        int len = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            while (set[s.charAt(r)] > 0) {
+                set[s.charAt(l)]--;
+                l++;
+            }
+            set[s.charAt(r)]++;
+            len = Math.max(len, r - l + 1);
+        }
+        return len;
+    }
+}
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int len = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            while (set.contains(s.charAt(r)))
+                set.remove(s.charAt(l++));
+            set.add(s.charAt(r));
+            len = Math.max(len, r - l + 1);
+        }
+        return len;
+    }
+}
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
@@ -10,28 +42,6 @@ class Solution {
                 l++;
             }
             set.add(chars[r]);
-            max = Math.max(max, r-l+1);
-        }
-        return max;
-    }
-}
-
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
-        int max = 0;
-        char[] chars = s.toCharArray();
-        int l=0;
-        for (int r=0; r<chars.length; r++) {
-            if (set.contains(chars[r])) {
-                while (chars[l] != chars[r]) {
-                    set.remove(chars[l]);
-                    l++;
-                }
-                l++;
-            } else {
-                set.add(chars[r]);
-            }
             max = Math.max(max, r-l+1);
         }
         return max;
