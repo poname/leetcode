9480877bc52d6075f3f415221f80fe1e976ccd86
Dan <17553473+poname@users.noreply.github.com>
2025-06-26 19:46:15 -0400

    Update 11.Container_With_Most_Water.java

--- 11.Container_With_Most_Water.java
+++ 11.Container_With_Most_Water.java
@@ -16,3 +16,18 @@ class Solution {
         return max;
     }
 }
+
+// Brute-Force
+// time: O(n^2)
+// space: O(1)
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return max;
+    }
+}
