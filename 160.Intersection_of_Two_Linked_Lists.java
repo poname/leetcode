49d32add4e690884067fe9e4132ec31a657bfacc
Dan <17553473+poname@users.noreply.github.com>
2025-07-03 21:21:50 -0400

    Create 160.Intersection_of_Two_Linked_Lists.java

---
+++ 160.Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,36 @@
+// two pass
+// time: O(n)
+// space: O(1)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0;
+        ListNode curA = headA;
+        while (curA != null) {
+            lenA++;
+            curA = curA.next;
+        }
+        int lenB = 0;
+        ListNode curB = headB;
+        while (curB != null) {
+            lenB++;
+            curB = curB.next;
+        }
+        if (lenB > lenA) {
+            for (int i = 0; i < (lenB - lenA); i++) {
+                headB = headB.next;
+            }
+        }
+        if (lenA > lenB) {
+            for (int i = 0; i < (lenA - lenB); i++) {
+                headA = headA.next;
+            }
+        }
+        while (headA != null && headB != null) {
+            if (headA == headB)
+                return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
