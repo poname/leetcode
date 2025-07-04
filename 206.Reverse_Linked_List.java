3e3a8cc8ea52b222806a002072d040ab0d9776f7
Dan <17553473+poname@users.noreply.github.com>
2025-07-03 20:43:00 -0400

    Update 206.Reverse_Linked_List.java

--- 206.Reverse_Linked_List.java
+++ 206.Reverse_Linked_List.java
@@ -15,6 +15,17 @@ class Solution {
 
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode new_head = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return new_head;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
         if (head == null) {
             return null;
         }
