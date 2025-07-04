b6f1a1a1d6451654dfe92a94c0d24bbd7758106d
Dan <17553473+poname@users.noreply.github.com>
2025-07-03 20:45:12 -0400

    Update 206.Reverse_Linked_List.java

--- 206.Reverse_Linked_List.java
+++ 206.Reverse_Linked_List.java
@@ -13,6 +13,8 @@ class Solution {
     }
 }
 
+// time: O(n)
+// space: O(n) -> recursive stack
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null)
