3cb8dd7ba9d18f120a8a0a75fa3c37ad3f943859
Dan <17553473+poname@users.noreply.github.com>
2025-07-07 20:05:25 -0400

    Update 438.Find_All_Anagrams_in_a_String.java

--- 438.Find_All_Anagrams_in_a_String.java
+++ 438.Find_All_Anagrams_in_a_String.java
@@ -25,3 +25,35 @@ class Solution {
         return out;
     }
 }
+
+// optimized - no array compare
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int l = 0;
+        int[] sC = new int[26];
+        int[] pC = new int[26];
+        int have = 0;
+        int want = 0;
+        List<Integer> out = new ArrayList<>();
+        for (char c : p.toCharArray())
+            pC[c - 'a']++;
+        for (int x : pC)
+            want += (x > 0) ? 1 : 0;
+        for (int r = 0; r < s.length(); r++) {
+            int iR = s.charAt(r) - 'a';
+            sC[iR]++;
+            if (sC[iR] == pC[iR])
+                have++;
+            if (r - l + 1 == p.length()) {
+                if (have == want)
+                    out.add(l);
+                int iL = s.charAt(l) - 'a';
+                if (sC[iL] == pC[iL])
+                    have--;
+                sC[iL]--;
+                l++;
+            }
+        }
+        return out;
+    }
+}
