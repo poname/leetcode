d0158d92c5c724ca9f63f3726567e1db4952869b
Dan <17553473+poname@users.noreply.github.com>
2025-07-08 20:54:46 -0400

    Update 35.Search_Insert_Position.java

--- 35.Search_Insert_Position.java
+++ 35.Search_Insert_Position.java
@@ -2,6 +2,23 @@
 // space: O(1)
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (target < nums[m]) {
+                r = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return l;
+    }
+}
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
         int l = 0, r = nums.length;
         while (l < r) {
             int m = (l + r) / 2;
