a72719ef7b64064ff831cf4795a0db57ee8a51c6
Dan <17553473+poname@users.noreply.github.com>
2025-07-08 20:35:38 -0400

    Create 35.Search_Insert_Position.java

---
+++ 35.Search_Insert_Position.java
@@ -0,0 +1,16 @@
+// time: O(log(n))
+// space: O(1)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l;
+    }
+}
