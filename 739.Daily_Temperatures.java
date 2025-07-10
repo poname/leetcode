e23ec1e29972bcb43560f5ae9420dce46421c110
Dan <17553473+poname@users.noreply.github.com>
2025-07-09 22:47:58 -0400

    Update 739.Daily_Temperatures.java

--- 739.Daily_Temperatures.java
+++ 739.Daily_Temperatures.java
@@ -1,3 +1,38 @@
+// time: O(n)
+// space: O(n)
+// stack
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] out = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (stack.size() > 0 && temperatures[i] > temperatures[stack.peek()]) {
+                out[stack.peek()] = i - stack.pop();
+            }
+            stack.add(i);
+        }
+        return out;
+    }
+}
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] out = new int[n];
+        int[] stack = new int[n];
+        int top = -1;
+        for (int i = 0; i < n; i++) {
+            while (top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+                out[stack[top]] = i - stack[top];
+                top--;
+            }
+            top++;
+            stack[top] = i;
+        }
+        return out;
+    }
+}
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
