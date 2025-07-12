c170fd366d13ac55c608c0876b3253c467fa8dcb
Dan <17553473+poname@users.noreply.github.com>
2025-07-12 19:25:05 -0400

    Update 56.Merge_Intervals.java

--- 56.Merge_Intervals.java
+++ 56.Merge_Intervals.java
@@ -1,3 +1,23 @@
+// sorting
+// time: O(nlog(n))
+// space: O(sort space) -> O(1) or O(n)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> merged = new ArrayList<>();
+        merged.add(intervals[0]);
+        for (int[] t : intervals) {
+            int[] base = merged.get(merged.size()-1);
+            if (t[0] <= base[1]) {
+                base[1] = Math.max(base[1], t[1]);
+            } else {
+                merged.add(t);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
+
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
