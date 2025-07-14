3c479ceeca99d81891be6e19adfc2d1563d2253e
Dan <17553473+poname@users.noreply.github.com>
2025-07-14 15:57:33 -0400

    Update 253.Meeting_Rooms_II.java

--- 253.Meeting_Rooms_II.java
+++ 253.Meeting_Rooms_II.java
@@ -10,10 +10,6 @@
  */
 
 public class Solution {
-    /**
-     * @param intervals: an array of meeting time intervals
-     * @return: the minimum number of conference rooms required
-     */
     public int minMeetingRooms(List<Interval> intervals) {
         if (intervals == null || intervals.size() == 0) {
             return 0;
@@ -38,52 +34,74 @@ public class Solution {
     }
 }
 
-/**
- * Two Pointer
- */
-
+// min heap
+// time: O()
+// space: O()
 public class Solution {
-    /**
-     * @param intervals: an array of meeting time intervals
-     * @return: the minimum number of conference rooms required
-     */
     public int minMeetingRooms(List<Interval> intervals) {
-        if (intervals == null || intervals.size() == 0) {
-            return 0;
+        intervals.sort((a, b) -> a.start - b.start);
+        Queue<Interval> minH = new PriorityQueue<>((a, b) -> a.end - b.end);
+        int max = 0;
+        for (Interval iv : intervals) {
+            while (minH.size() > 0 && minH.peek().end <= iv.start) {
+                minH.poll();
+            }
+            minH.add(iv);
+            max = Math.max(max, minH.size());
         }
+        return max;
+    }
+}
+
+// two pointer - sort
+// time: O(nlog(n))
+// space: O(n)
+public class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
         int n = intervals.size();
-        // Write your code here
         int[] start = new int[n];
         int[] end = new int[n];
-        
-        for (int i=0; i<n; i++) {
-            Interval iv = intervals.get(i);
-            start[i] = iv.start;
-            end[i] = iv.end;
+        for (int k=0; k<n; k++) {
+            Interval iv = intervals.get(k);
+            start[k] = iv.start;
+            end[k] = iv.end;
         }
-
         Arrays.sort(start);
         Arrays.sort(end);
-
-        int s=0;
-        int e=0;
-        int curr = 0;
+        int i=0, j=0;
         int max = 0;
-        while (s<n && e<n) {
-            if (start[s] < end[e]) {
-                curr++;
-                s++;
-                max = Math.max(max, curr);
-            } else if (start[s] > end[e]) {
-                curr--;
-                e++;
-                max = Math.max(max, curr);
+        int ov = 0;
+        while (i<n && j<n) {
+            if (start[i] < end[j]) {
+                ov++;
+                i++;
             } else {
-                s++;
-                e++;
+                ov--;
+                j++;
             }
+            max = Math.max(max, ov);
         }
+        return max;
+    }
+}
 
+// greedy - sort
+// time: O(nlog(n))
+// space: O(n)
+public class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        List<int[]> time = new ArrayList<>();
+        for (Interval iv : intervals) {
+            time.add(new int[]{iv.start, 1});
+            time.add(new int[]{iv.end, -1});
+        }
+        time.sort((a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        int ov=0;
+        int max=0;
+        for (int[] t : time) {
+            ov += t[1];
+            max = Math.max(max, ov);
+        }
         return max;
     }
 }
