1c59fd36cd8b54dc1314522bbb99d5f1684876b7
Dan <17553473+poname@users.noreply.github.com>
2025-07-14 20:09:02 -0400

    Update 303.Range_Sum_Query.java

--- 303.Range_Sum_Query.java
+++ 303.Range_Sum_Query.java
@@ -1,17 +1,18 @@
+// time: O(1) for query and O(n) for constructor
+// space: O(n) for constructor
 class NumArray {
 
-    int[] prefSum;
+    int[] sum;
 
     public NumArray(int[] nums) {
-        prefSum = new int[nums.length];
-        int sum = 0;
-        for (int i=0; i<nums.length; i++) {
-            sum += nums[i];
-            prefSum[i] = sum;
+        sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
         }
     }
-    
+
     public int sumRange(int left, int right) {
-        return prefSum[right] - (left==0 ? 0 :  prefSum[left-1]);
+        return sum[right] - ((left > 0) ? sum[left - 1] : 0);
     }
 }
