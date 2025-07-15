a5e7b12ba1da21f73a97bf6077d6dcfa30a411e9
Dan <17553473+poname@users.noreply.github.com>
2025-07-15 15:11:58 -0400

    Update 226.Invert_Binary_Tree.java

--- 226.Invert_Binary_Tree.java
+++ 226.Invert_Binary_Tree.java
@@ -1,29 +1,55 @@
+// time: O(n)
+// space: O(n)
+// note: swap and add/push/call order doesn't matter
+
+// DFS - recursive
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
+        if (root != null) {
+            invertTree(root.left);
+            invertTree(root.right);
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
         }
-
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-
-        invertTree(root.right);
-        invertTree(root.left);
-
         return root;
     }
 }
 
+// BFS - queue
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            TreeNode front = queue.poll();
+            if (front != null) {
+                TreeNode temp = front.left;
+                front.left = front.right;
+                front.right = temp;
+                queue.add(front.left);
+                queue.add(front.right);
+            }
         }
-        TreeNode left = invertTree(root.right);
-        TreeNode right = invertTree(root.left);
-        root.left = left;
-        root.right = right;
+        return root;
+    }
+}
+
+// iterative DFS - stack
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (stack.size() > 0) {
+            TreeNode top = stack.pop();
+            if (top != null) {
+                TreeNode temp = top.left;
+                top.left = top.right;
+                top.right = temp;
+                stack.push(top.left);
+                stack.push(top.right);
+            }
+        }
         return root;
     }
 }
