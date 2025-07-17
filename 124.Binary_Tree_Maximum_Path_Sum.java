eac93548ac8586d4113efa34f3960fe2d1bf7cc2
Dan <17553473+poname@users.noreply.github.com>
2025-07-17 15:34:27 -0400

    Update 124.Binary_Tree_Maximum_Path_Sum.java

--- 124.Binary_Tree_Maximum_Path_Sum.java
+++ 124.Binary_Tree_Maximum_Path_Sum.java
@@ -1,3 +1,22 @@
+// time: O(n)
+// space: O(n)
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxL = Math.max(0, dfs(root.left));
+        int maxR = Math.max(0, dfs(root.right));
+        max = Math.max(max, maxL + root.val + maxR);
+        return root.val + Math.max(maxL, maxR);
+    }
+}
+
 class Solution {
     
     public int maxPathSum(TreeNode root) {
@@ -14,5 +33,24 @@ class Solution {
         int rightMax = Math.max(0, dfs(root.right, res));
         res[0] = Math.max(res[0], leftMax+rightMax+root.val);
         return root.val + Math.max(leftMax, rightMax);
+    }
+}
+
+// time: O(n^2)
+// space: O(n)
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int here = Math.max(0, dfs(root.left)) + Math.max(0, dfs(root.right)) + root.val;
+        return Math.max(here, Math.max(maxPathSum(root.left), maxPathSum(root.right)));
+    }
+
+    int dfs(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        return root.val + Math.max(0, Math.max(dfs(root.left), dfs(root.right)));
     }
 }
