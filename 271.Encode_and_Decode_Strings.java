a8ec55e29db5ecf7746042e35f38c57ca42061eb
Dan <17553473+poname@users.noreply.github.com>
2025-07-20 14:56:17 -0400

    Update 271.Encode_and_Decode_Strings.java

--- 271.Encode_and_Decode_Strings.java
+++ 271.Encode_and_Decode_Strings.java
@@ -1,69 +1,41 @@
-// time: O()
-// space: O()
+// m is the sum of lengths of all the strings 
+// n is the number of strings
+// k is the average length of all teh strings
+// m ~ k * n
+// header size: 4 ~ log(k)
+// header overhead: n * 4 ~ n * log(k)
+// time: 
+//       encode -> O(m + n*4) ~ O(n * (k+log(k))
+//       decode -> same
+// space: 
+//       encode -> O(m + n*4) ~ O(n * (k+log(k))
+//       decode -> same
+// basically O(m) or O(nk)
 public class Solution {
-    /*
-     * @param strs: a list of strings
-     * @return: encodes a list of strings to a single string.
-     */
     public String encode(List<String> strs) {
-        // write your code here
         StringBuilder sb = new StringBuilder();
         for (String s : strs) {
             sb.append(s.length());
-            sb.append(":");
+            sb.append("#");
             sb.append(s);
         }
         return sb.toString();
     }
 
-    /*
-     * @param str: A string
-     * @return: decodes a single string to a list of strings
-     */
     public List<String> decode(String str) {
-        // write your code here
-        int s=0;
-        int e=0;
-        List<String> ans = new ArrayList<>();
-        while (s<str.length()) {
-            while (str.charAt(e) != ':') e++;
-            int len = Integer.parseInt(str.substring(s, e));
-            ans.add(str.substring(e+1, e+len+1));
-            s = e+len+1;
-            e = e+len+1;
+        List<String> out = new ArrayList<>();
+        int i=0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int len = Integer.parseInt(str.substring(i, j));
+            i = j+1;
+            j = i+len;
+            out.add(str.substring(i, j));
+            i = j;
         }
-        return ans;
-    }
-}
-
-public class Solution {
-    /*
-     * @param strs: a list of strings
-     * @return: encodes a list of strings to a single string.
-     */
-    public String encode(List<String> strs) {
-        // write your code here
-        StringBuilder sb = new StringBuilder();
-        for (String s : strs) {
-            sb.append(s.length()+":");
-            sb.append(s);
-        }
-        return sb.toString();
-    }
-
-    /*
-     * @param str: A string
-     * @return: decodes a single string to a list of strings
-     */
-    public List<String> decode(String str) {
-        // write your code here
-        List<String> strs = new ArrayList<>();
-        while (str.length() > 0) {
-            int idx = str.indexOf(":");
-            int len = Integer.valueOf(str.substring(0, idx));
-            strs.add(str.substring(idx+1, idx+1+len));
-            str = str.substring(idx+len+1);
-        }
-        return strs;
+        return out;
     }
 }
