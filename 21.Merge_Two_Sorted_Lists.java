bfa84b9a3ac4b8acc3412f73222ddedcab471e72
Dan <17553473+poname@users.noreply.github.com>
2025-07-20 17:58:03 -0400

    Update 21.Merge_Two_Sorted_Lists.java

--- 21.Merge_Two_Sorted_Lists.java
+++ 21.Merge_Two_Sorted_Lists.java
@@ -43,32 +43,3 @@ class Solution {
         }
     }
 }
-
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode();
-        rec(list1, list2, dummy);
-        return dummy.next;
-    }
-
-    ListNode rec(ListNode list1, ListNode list2, ListNode head) {
-        if (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                head.next = list1;
-                return rec(list1.next, list2, head.next);
-            } else {
-                head.next = list2;
-                return rec(list1, list2.next, head.next);
-            }
-        }
-        if (list1 != null) {
-            head.next = list1;
-            return null;
-        }
-        if (list2 != null) {
-            head.next = list2;
-            return null;
-        }
-        return null;
-    }
-}
