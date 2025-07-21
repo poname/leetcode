94f570bb9f4405a134d34c69daff286b840361e9
Dan <17553473+poname@users.noreply.github.com>
2025-07-21 19:00:47 -0400

    Update 100.Same_Tree.java

--- 100.Same_Tree.java
+++ 100.Same_Tree.java
@@ -1,3 +1,6 @@
+// parallel DFS
+// time: O(n)
+// space: O(h)
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
@@ -9,5 +12,30 @@ class Solution {
         }
         
         return false;
+    }
+}
+
+// parallel BFS
+// time: O(n)
+// space: O(n)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> pq = new LinkedList<>();
+        pq.add(p);
+        Queue<TreeNode> qq = new LinkedList<>();
+        qq.add(q);
+        while (pq.size() > 0 && qq.size() > 0) {
+            TreeNode pf = pq.poll();
+            TreeNode qf = qq.poll();
+            if (pf == null && qf == null)
+                continue;
+            if (pf == null || qf == null || pf.val != qf.val)
+                return false;
+            pq.add(pf.left);
+            pq.add(pf.right);
+            qq.add(qf.left);
+            qq.add(qf.right);
+        }
+        return pq.size() == qq.size();
     }
 }
