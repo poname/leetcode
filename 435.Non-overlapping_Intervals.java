3c88e6aef421a64adb9b18f50dd011e8aeb3a08f
Dan <17553473+poname@users.noreply.github.com>
2025-07-25 14:48:45 -0400

    Update 435.Non-overlapping_Intervals.java

--- 435.Non-overlapping_Intervals.java
+++ 435.Non-overlapping_Intervals.java
@@ -1,17 +1,42 @@
 // |-----|----------|
 // |----------|-----|
+
+// Greedy - sort by end
+// time: O(log(n))
+// space: O(1) or O(n) depending on the sort
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[1]-b[1]);
-        int overlap = 0;
-        int endSoFar = intervals[0][0]; // or Integer.MIN_VALUE;
-        for (int[] ival : intervals) {
-            if (endSoFar <= ival[0]) {
-                endSoFar = ival[1];
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int prevEnd = intervals[0][0]; // or Integer.MIN_VALUE;
+        int remove = 0;
+        for (int[] iv : intervals) {
+            if (iv[0] < prevEnd) {
+                remove++;
+                // prevEnd = Math.min(prevEnd, iv[1]);
             } else {
-                overlap++;
+                prevEnd = iv[1];
             }
         }
-        return overlap;
+        return remove;
+    }
+}
+
+// Greedy - sort by start
+// time: O(log(n))
+// space: O(1) or O(n) depending on the sort
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int prevEnd = intervals[0][0]; // or Integer.MIN_VALUE;
+        int remove = 0;
+        for (int[] iv : intervals) {
+            if (iv[0] < prevEnd) {
+                remove++;
+                prevEnd = Math.min(prevEnd, iv[1]);
+            } else {
+                prevEnd = iv[1];
+            }
+        }
+        return remove;
     }
 }
