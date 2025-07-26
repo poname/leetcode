de72b946ac94dd38efd21aee62c4f73714e425de
Dan <17553473+poname@users.noreply.github.com>
2025-07-26 14:06:03 -0400

    Update 53.Maximum_Subarray.java

--- 53.Maximum_Subarray.java
+++ 53.Maximum_Subarray.java
@@ -1,3 +1,6 @@
+// Kadane - greedy
+// time: O(n)
+// space: O(1)
 class Solution {
     public int maxSubArray(int[] nums) {
         int max = nums[0];
@@ -5,6 +8,20 @@ class Solution {
         for (int n : nums) {
             currMax = n + Math.max(0, currMax);
             max = Math.max(max, currMax);
+        }
+        return max;
+    }
+}
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int n : nums) {
+            if (sum < 0) { // start a new array from n
+                sum = 0;
+            }
+            sum += n;
+            max = Math.max(max, sum);
         }
         return max;
     }
