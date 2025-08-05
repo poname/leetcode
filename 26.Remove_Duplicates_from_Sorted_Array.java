bf749ec9570230cff01449cf762faf950fa54978
Dan <17553473+poname@users.noreply.github.com>
2025-08-05 19:32:20 -0400

    Update 26.Remove_Duplicates_from_Sorted_Array.java

--- 26.Remove_Duplicates_from_Sorted_Array.java
+++ 26.Remove_Duplicates_from_Sorted_Array.java
@@ -1,3 +1,4 @@
+// two pointers
 // time: O(n)
 // space: O(1)
 class Solution {
@@ -11,5 +12,22 @@ class Solution {
             nums[l] = nums[r];
         }
         return l + 1;
+    }
+}
+
+// sorted set
+// time: O(nlog(n))
+// space: O(k)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new TreeSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        int i = 0;
+        for (int n : set) {
+            nums[i++] = n;
+        }
+        return set.size();
     }
 }
